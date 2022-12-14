a03cae816e1aed541afec1cfaec25e197bb73a9d
AntSenokosov <a.y.senokosov@gmail.com>
2022-12-15 02:39:26 +0300

    Add solution and project WpfApp with settings

---
+++ Desktop/WPFApp/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Windows;
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly)
+
+    // where theme specific resource dictionaries are located
+    // (used if a resource is not found in the page,
+    // or application resource dictionaries)
+     // where the generic resource dictionary is located
+    // (used if a resource is not found in the page,
+    // app, or any theme specific resource dictionaries)
+]
